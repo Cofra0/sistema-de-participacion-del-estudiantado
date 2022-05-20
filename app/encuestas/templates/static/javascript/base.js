82059d719e1c0a7d6f2a60bad6531353e872f63a
BHerreraZ <benjaminherrera@ug.uchile.cl>
2022-05-20 21:22:32 +0000

    agregando vista principal

---
+++ app/encuestas/templates/static/javascript/base.js
@@ -0,0 +1,25 @@
+let user_name = document.getElementById("user_name");
+    
+user_name.onclick = function(){
+
+    let close_session = document.getElementById("close_session");
+    let arrow = document.getElementById("arrow");
+    
+
+    if (close_session.className == "close_session"){
+        close_session.classList.replace("close_session", "close_session_nv");
+        arrow.style.transform = "rotate(0deg)";
+        user_name.style.background = "#d1cb85";
+    }
+
+    else{
+        close_session.classList.replace("close_session_nv", "close_session" );
+        arrow.style.transform = "rotate(180deg)";
+        user_name.style.background = "#a9a46c";
+    }
+};
+
+
+let en = document.getElementById("encuestas");
+let mien = document.getElementById("mis_encuestas");
+let puen = document.getElementById("publicar");
--- app/encuestas/views.py
+++ app/encuestas/views.py
@@ -1,8 +1,36 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template.loader import get_template
+from django.shortcuts import render, redirect
+from encuestas.models import *
+from django.contrib.auth import authenticate, login, logout
 
 # Renderiza la pagina principal de encuestas.
 @login_required
 def main(request):
     return render(request, "main.html")
+
+
+# Create your views here.
+
+# Vista de la pagina principal
+def encuestas(request):  # the index view
+    encuestasDisponibles = Encuesta.objects.filter(activa=True).order_by(
+        "-puntos_encuesta"
+    )  # Se filtran la encuestas disponibles y se ordenan decrecientemente por puntos
+    return render(request, "encuestas/index.html", {"encuestas": encuestasDisponibles})
+
+
+# Vista del resumen de encuestas creadas y respondidas por el usuario
+def mis_encuestas(request):
+    return render(request, "encuestas/missing.html", {})
+
+
+# Vista del formulario para publicar una encuesta
+def publicar_encuesta(request):
+    return render(request, "encuestas/missing.html", {})
+
+
+# Vista donde la encuesta está incertada
+def encuesta(request):
+    return render(request, "encuestas/encuesta.html", {})

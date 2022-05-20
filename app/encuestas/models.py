616e189bc6c3574b79f237cb1d7b2e88d344af7d
G-Urrea <g.urrea.mansilla@gmail.com>
2022-05-20 21:53:18 +0000

    Modificado modelo de encuesta

--- app/encuestas/models.py
+++ app/encuestas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -40,6 +41,45 @@ class Encuesta(models.Model):
 
     activa = models.BooleanField(verbose_name="Determina si la encuesta está activa o no", default=True)
 
+    hash = models.CharField(
+        verbose_name = 'Código para reclamar puntos de encuesta (Máximo de 200 carácteres)',
+        max_length=255,
+        default = 'Paral3l3pipedo' # Hash por defecto (De otra forma no me dejaba hacer la migración uwu)
+    )
+
+
+    @property
+    def active(self):
+        '''
+        Propiedad para saber si una encuesta está activa, si ha pasado el plazo entonces retornará False 
+        y se actualizará la base de datos.
+        De otra forma se retornará el campo "activa" de la BBDD.
+        '''
+        # Primero cachar si hay plazo
+        if self.plazo<datetime.now():
+            # No hay plazo, se cierra la encuesta.
+            self.closing_survey()
+            return False
+        # El plazo no ha acabado, se retorna el valor de "activa" (si la encuesta fue eliminada será false)
+        return self.activa
+
+
+    def closing_survey(self):
+        '''
+        Llamar cuando se elimine una encuesta o se termine el plazo.
+        Devuelve los puntos restantes de la encuesta al creador de esta y deja el campo "activa" en False.
+        '''
+        # Si la encuesta aún tiene puntos asignados
+        if self.puntos_totales>0:
+            creador = self.creador
+            creador.puntos+=self.puntos_totales # Se le retornan los puntos al creador
+            self.puntos_totales = 0 # Se quitan los puntos a la encuesta   
+            
+            # Se guardan los cambios
+            creador.save()
+        self.puntos_encuesta = 0 # Por si las moscas, la encuesta ya no debería poder repartir puntos
+        self.activa = False # Se cierra la encuesta
+        self.save()
 
 class Responde(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)

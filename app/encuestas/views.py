df77cd6f04b3435f52f32b0392e99ddbb41e5ad1
Cofra0 <hitorigamicofra@gmail.com>
2022-05-20 22:12:43 -0400

    probando

--- app/encuestas/views.py
+++ app/encuestas/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from encuestas.models import Encuesta
 
 
 # Renderiza la pagina principal de encuestas.
 @login_required
-def main(request):
-    return render(request, "main.html")
+def encuesta_seleccionada(request):
+    id_encuesta = request.GET.get("id", "")
+    datos_encuesta = Encuesta.objects.get(id=id_encuesta)
+    return render(request, "encuesta_seleccionada.html", datos_encuesta)

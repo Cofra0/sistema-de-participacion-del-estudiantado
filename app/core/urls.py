7ef39bb4a29efa47d0e73c06fd3c263df2ba8ad0
Cofra0 <hitorigamicofra@gmail.com>
2022-06-11 21:24:44 -0400

    Prueba Captcha

--- app/core/urls.py
+++ app/core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("sso/", include("sso.urls")),
     path("", include("encuestas.urls")),
+    path("captcha/", include("captcha.urls")),
 ]

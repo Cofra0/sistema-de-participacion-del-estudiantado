f26584f66333411da8338f6620e159231990694e
tetrapacker238 <alejandro.salas@live.cl>
2022-06-30 23:23:49 +0000

    popout on trying to navegate or quit the app when user hasn't answered correctly

--- app/staticfiles/js/alert.js
+++ app/staticfiles/js/alert.js
@@ -1,9 +1,19 @@
 /*JavaScript para alertar al usuario si no ha respondido la encuesta*/
-console.log("cargo al menos");
-window.onbeforeunload = function() {
-    console.log("en la functionnnnnnnnnnnnn");
-    const respondido = document.getElementById("respondido");
-    if (!(respondido)) {
-        return "Aún no has respondido correctamente la encuesta. ¿Deseas salir de todas maneras?";
-    }
-}
+const confirmationMessage = "Aún no se ha respondido la encuesta correctamente." + 
+ "¿Deseas salir de todas maneras?";
+let respondido = document.getElementById("respondido");
+respondido = respondido.value === "True";
+/*
+Seleccionamos todos los tags 'a' y le agregamos una función que maneje
+si la encuesta fue respondida o no
+*/
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        if (!respondido) {
+            const res = window.confirm(confirmationMessage);
+            if (!res) { //Si el cliente cancela la salida a la página, se cancela el redirect
+                e.preventDefault();
+            }
+        }
+    });
+  });
